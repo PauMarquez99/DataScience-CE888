be0638ad0cca4aaac5dc43ccd39c6c71eeaa0bab
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2022-04-27 20:26:18 +0100

    Update CE888-Functions.py

--- Assignments/CE888 Assignment 2/Datasets Notebooks/CE888-Functions.py
+++ Assignments/CE888 Assignment 2/Datasets Notebooks/CE888-Functions.py
@@ -18,3 +18,17 @@ def pehe(effect_true, effect_pred):
     :return: PEHE
     """
     return np.sqrt(np.mean(np.power((np.array(effect_true)-np.array(effect_pred)),2)))
+
+def get_ps_weights(clf, x, t):
+    ti = np.squeeze(t)
+    clf.fit(x, ti)
+    ptx = clf.predict_proba(x).T[1].T + 0.0001 # add a small value to avoid dividing by 0
+    # Given ti and ptx values, compute the weights wi (see formula above):
+    wi = (ti/ptx)+((1-ti)/(1-ptx))
+    
+    return wi
+
+def mean_ci(data, ci=0.95):
+    l_mean = np.mean(data)
+    lower, upper = st.t.interval(ci, len(data)-1, loc=l_mean, scale=st.sem(data))
+    return l_mean, lower, upper
